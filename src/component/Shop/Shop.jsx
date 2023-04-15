f8867941de21ccd9f99f8d512314848e4e8ef795
sadakatsrabon <md.hossain.sadakat@gmail.com>
2023-04-15 20:10:45 +0600

    local storate added

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -15,8 +15,16 @@ const Shop = () => {
     }, []);
     useEffect( ()=> {
         const storedCart = getShoppingCart();
-        console.log(storedCart);
-    },[]);
+        // step :1
+        for (const id in storedCart){
+            // step : 2 : get the product by using id;
+            const addedProduct = products.find(product =>product.id === id)
+            // step : 3: get quantity of the product;
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            console.log(addedProduct);
+        } 
+    },[products]);
 
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];

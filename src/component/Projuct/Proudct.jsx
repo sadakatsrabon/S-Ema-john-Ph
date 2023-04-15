82529eea86937dade4718da9e576b698b55e5eef
sadakatsrabon <md.hossain.sadakat@gmail.com>
2023-04-15 11:15:47 +0600

    button-added

--- src/component/Projuct/Proudct.jsx
+++ src/component/Projuct/Proudct.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
-const Product = ({ product }) => {
-    const { img, name, seller, ratings, price } = product;
+const Product = (props) => {
+    console.log(props)
+    const { img, name, seller, ratings, price } = props.product;
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +17,7 @@ const Product = ({ product }) => {
                 <p>Manufacturar : {seller}</p>
                 <p>Rating : {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add To Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add To Cart <FontAwesomeIcon icon={faShoppingCart} /></button>
         </div>
     );
 };
--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -4,12 +4,19 @@ import Proudct from '../Projuct/Proudct';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data));
     }, []);
+
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+    
     return (
         <div className='shop-container'>
             {/* Products container  */}
@@ -18,12 +25,14 @@ const Shop = () => {
                     products.map(product => <Proudct
                         key={product.id}
                         product={product}
+                        handleAddToCart={handleAddToCart}
                     ></Proudct>)
                 }
             </div>
             {/* Carts container  */}
             <div className="carts-container">
                 <h3>Order summury</h3>
+                <p>Selected Items: {cart.length}</p>
             </div>
         </div>
     );

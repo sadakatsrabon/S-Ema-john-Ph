927d9c4e0486c0418e6d13156d47368c3514ce0d
sadakatsrabon <md.hossain.sadakat@gmail.com>
2023-04-16 06:18:12 +0600

    shoe data from cart

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Cart.css';
-const Cart = ({cart}) => {
+const Cart = ({ cart }) => {
     // const Cart = (props) => {
     // const cart = props.cart; system :2
     // const {cart} = props; system : 3
@@ -8,17 +8,22 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
-    for(const product of cart){
-        totalPrice = totalPrice + product.price;
-        totalShipping = totalShipping + product.shipping;
+    let quantity = 0;
+    for (const product of cart) {
+        // if (product.quantity === 0) {
+        //     product.quantity === 1
+        // };
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping * product.quantity;
+        quantity = quantity + product.quantity;
     }
-    const tax = (totalPrice *7)/100
+    const tax = (totalPrice * 7) / 100;
     const grandTotal = totalPrice + totalShipping + tax;
 
     return (
         <div className='cart'>
             <h3>Order summary</h3>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: $ {totalPrice}</p>
             <p>Total Shipping:${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
